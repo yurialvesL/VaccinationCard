55a30329764fa94981c0a64b011f3573a58ca22f
Yuri <Yuri@YURI-PC>
2025-08-30 13:40:00 -0300

    feat: created Person repository to use crud functions

---
+++ VaccinationCard.Domain/Interfaces/IPersonRepository.cs
@@ -0,0 +1,49 @@
+﻿using VaccinationCard.Domain.Entities;
+
+namespace VaccinationCard.Domain.Interfaces;
+
+
+/// <summary>
+/// Repository interface for Person entity operations
+/// </summary>
+public interface IPersonRepository
+{
+    /// <summary>
+    /// Creates a new person in the repository
+    /// </summary>
+    /// <param name="person">The person to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created person</returns>
+    Task<Person> CreatePersonAsync(Person person, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a person by their unique identifier
+    /// </summary>
+    /// <param name="personId">The unique identifier of the person</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The person if found, null otherwise</returns>
+    Task<Person?> GetPersonByIdAsync(Guid personId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Retrieves a paginated list of persons from the repository
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns></returns>
+    Task<List<Person>> GetAllPersonAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Deletes a person from the repository
+    /// </summary>
+    /// <param name="personId">The unique identifier of the user to delete</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>True if the user was deleted, false if not found</returns>
+    Task<bool> DeletePersonAsync(Guid personId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates an existing person in the repository
+    /// </summary>
+    /// <param name="person">The person to update</param>
+    /// <param name="cancellationToken">Cancellation token</param>  
+    /// <returns>The updated person if successful, null if the person does not exist</returns>
+    Task<Person?> UpdatePersonAsync(Person person, CancellationToken cancellationToken = default);
+}
---
+++ VaccinationCard.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccinationCard.Domain.Entities;
+using VaccinationCard.Domain.Interfaces;
+using VaccinationCard.Infrastructure.Context;
+
+namespace VaccinationCard.Infrastructure.Repositories;
+
+
+/// <summary>
+/// Implementation of IPersonRepository using Entity Framework Core
+/// </summary>
+public class PersonRepository(ApplicationDbContext dbContext) : IPersonRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    /// <summary>
+    /// Creates a new Person in the repository
+    /// </summary>
+    /// <param name="person">The person to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created person</returns>
+    public async Task<Person> CreatePersonAsync(Person person,CancellationToken cancellationToken)
+    {
+        _dbContext.Persons.Add(person);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return person;
+    }
+
+    /// <summary>
+    /// Retrieves a person by their unique identifier
+    /// </summary>
+    /// <param name="id">The unique identifier of the person</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The person if found, null otherwise</returns>
+    public async Task<Person?> GetPersonByIdAsync(Guid personId, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Persons.FirstOrDefaultAsync(o => o.Id == personId, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves all person records from the repository
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The person if found, null otherwise</returns>
+    public async Task<List<Person>> GetAllPersonAsync(CancellationToken cancellationToken)
+    {
+        return await _dbContext.Persons.ToListAsync(cancellationToken: cancellationToken);
+    }
+
+    /// <summary>
+    /// Deletes a person by their unique identifier
+    /// </summary>
+    /// <param name="personId">person unique identifier</param>
+    /// <param name="cancellationToken">Cancellation token</param>  
+    /// <returns>True if the person was deleted, false otherwise</returns>
+    public async Task<bool> DeletePersonAsync(Guid personId, CancellationToken cancellationToken)
+    {
+        var deleted = await _dbContext.Persons
+           .Where(i => i.Id == personId)
+           .ExecuteDeleteAsync(cancellationToken: cancellationToken);
+
+        return deleted > 1;
+    }
+
+    /// <summary>
+    /// Updates an existing person in the repository
+    /// </summary>
+    /// <param name="person">person unique identifier</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The updated person if found, null otherwise</returns>
+    public async Task<Person?> UpdatePersonAsync(Person person, CancellationToken cancellationToken)
+    {
+        _dbContext.Persons.Update(person);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return await _dbContext.Persons
+            .FirstOrDefaultAsync(i => i.Id == person.Id, cancellationToken: cancellationToken);
+    }
+
+}
